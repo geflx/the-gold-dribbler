ccca82a1138b4b2d677c93a785850725cb0a2eae
Gabriel Félix <gabrielfelix2500@gmail.com>
2021-05-22 16:03:31 -0300

    Adding player movement.

---
+++ The Miner Problem/Assets/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Singleton
+
+	public static Player instance;
+
+	void Awake() {
+		if(instance != null) {
+			Debug.LogWarning ("More than one instance of Inventory found!");
+			return;
+		}
+		instance = this;
+	}
+
+	#endregion
+
+    // Characteristics
+    private float speed = 5f;
+    private float jumpForce = 6f;
+
+    // Movement
+    Vector2 motionVector;
+    public int maxJumps = 2;
+    public int jumpCounter = 2;
+
+    // Components
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    void move() 
+    {
+        moveHorizontally();
+
+        if(Input.GetKeyDown("space") && jumpCounter > 0)
+            jump();
+    }
+
+    void moveHorizontally ()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+        rigidBody.velocity = new Vector2(direction * speed, rigidBody.velocity.y);
+    }
+
+    void jump ()
+    {
+        rigidBody.velocity = Vector2.up * jumpForce;
+        jumpCounter--;
+    }
+
+    public void OnTriggerEnter2D (Collider2D collider)
+    {
+        if(collider.tag == "Ground")
+            jumpCounter = maxJumps;
+    }
+}
---
+++ The Miner Problem/Assets/Player_GroundCheck.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_GroundCheck : MonoBehaviour
+{
+    public void OnTriggerEnter2D (Collider2D collider)
+    {
+        if(collider.tag == "Ground")
+            Player.instance.jumpCounter = Player.instance.maxJumps;
+    }
+}
