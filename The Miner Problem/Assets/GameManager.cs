f3e14cc8d7358f35ede7849d08ad88cdf228ff58
Gabriel Félix <gabrielfelix2500@gmail.com>
2021-05-23 17:13:40 -0300

    Adding Jewel object.

---
+++ The Miner Problem/Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+
+	public static GameManager instance;
+
+	void Awake() {
+		if(instance != null) {
+			Debug.LogWarning ("More than one instance of Inventory found!");
+			return;
+		}
+		instance = this;
+	}
+
+	#endregion
+
+    public int horde;
+    public float[] hordeDuration = {20.0f, 15.0f};
+    public float intervalTime = 5f;
+
+    void Start ()
+    {
+        horde = 0;
+    }
+
+    public void handleGameOver ()
+    {
+        /* show something... */
+        Debug.Log("Game over!");
+    }
+}
---
+++ The Miner Problem/Assets/Jewel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jewel : MonoBehaviour
+{
+    public CircleCollider2D circleCollider;
+
+    public float speed;
+
+    private Vector2 targetPosition;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defineTargetPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //handleObjectLife();
+        move();
+    }
+
+    private void defineTargetPosition () 
+    {
+        targetPosition = Player.instance.transform.position;
+
+        Vector2 myPosition = gameObject.transform.position;
+
+        // Goes 10 times the distance between my position and player position (parametric line equation).
+        targetPosition.x = 10 * (targetPosition.x - myPosition.x) + myPosition.x;
+        targetPosition.y = 10 * (targetPosition.y - myPosition.y) + myPosition.y;
+    }
+
+    private float distanceFromPlayer ()
+    {
+        return Vector3.Distance(transform.position, Player.instance.transform.position);
+    }
+
+    private void handleObjectLife ()
+    {
+        if(distanceFromPlayer() > 30.0)
+            Destroy(gameObject);
+    }
+
+    private void move ()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+
+    public void OnTriggerEnter2D (Collider2D collider)
+    {
+        if (collider.tag == "Player")
+            GameManager.instance.handleGameOver();
+    }
+}
