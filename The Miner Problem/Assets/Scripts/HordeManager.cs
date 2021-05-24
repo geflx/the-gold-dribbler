e9039a738988d2b46bfa9d4702acddebd0f125c3
Gabriel Félix <gabrielfelix2500@gmail.com>
2021-05-24 17:16:23 -0300

    Adding HordeManager (Singleton) and IntervalTimer.

---
+++ The Miner Problem/Assets/Scripts/HordeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HordeManager : MonoBehaviour
+{
+    #region Singleton
+
+	public static HordeManager instance;
+
+	void Awake() {
+		if(instance != null) {
+			Debug.LogWarning ("More than one instance of Inventory found!");
+			return;
+		}
+		instance = this;
+	}
+
+	#endregion
+
+    public int horde;
+    public bool hordeOn, intervalOn;
+
+    void Start()
+    {
+        horde = 1;
+        hordeOn = false;
+        intervalOn = true;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ The Miner Problem/Assets/Scripts/IntervalTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntervalTimer : MonoBehaviour
+{
+    private float duration = 4.0f;
+
+    void Start()
+    {
+        HordeManager.instance.intervalOn = true;
+    }
+
+    void Update()
+    {
+        runClock();
+
+        if (duration < 0.0f)
+            OnTimeIsUp();
+    }
+
+    private void runClock ()
+    {
+        duration -= Time.deltaTime;
+    }
+
+    private void OnTimeIsUp ()
+    {
+        HordeManager.instance.intervalOn = false;
+
+        /* show message about next Horde in Game UI */
+
+        /* maybe this timer will need to talk with horde timer... */
+    }
+}
