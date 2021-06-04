9c182e8f97fec3079245c598a205c3ee612da66c
Gabriel Félix <gabrielfelix2500@gmail.com>
2021-06-04 11:15:41 -0300

    Adding credits and commands panel...

--- The Miner Problem/Assets/Scripts/Menu.cs
+++ The Miner Problem/Assets/Scripts/Menu.cs
@@ -5,7 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public Animator commandsAnim;
+    public GameObject commandsPanel;
+
+    public Animator creditsAnim;
+    public GameObject creditsPanel;
+
+    void Start ()
+    {
+        /* Activate Panels */
+        commandsPanel.SetActive(true);
+        creditsPanel.SetActive(true);
+    }
+
     public void LoadGame () {
         SceneManager.LoadScene("Arena");
+    }
+
+    public void ShowCommands (bool show)
+    {
+        commandsAnim.SetBool("isActive", show);
+    }
+
+    public void ShowCredits (bool show)
+    {
+        creditsAnim.SetBool("isActive", show);
     }
 }
